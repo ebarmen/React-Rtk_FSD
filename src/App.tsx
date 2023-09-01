7ec9a07dbdb01417092c6e032c22f2f9dc36f502
vladik <lordeonelove@gmail.com>
2023-09-01 17:44:54 +0500

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useRef, useState } from 'react'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const myRef = useRef(0)
+  const [state, setState] = useState<number>(0)
+
+  useEffect(() => {
+    let x = () => setState(window.scrollY);
+      window.addEventListener('scroll', x  )
+
+      return ()=> window.removeEventListener('scroll', x )
+  }, []);
+
+  const y = ()=> {
+    setState(state+1);
+    setState(state+1);
+    fetch('https://fakestoreapi.com/products/1').then((res)=> res.json()).then((res)=> console.log(res))
+    setState(state+1);
+    setState(state+1);
+    
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <button onClick={y}>xxxx</button>
+    <button onClick={()=> myRef.current= myRef.current+1}>yyyy</button>
+   {state}
+   {myRef.current}
+     </>
   )
 }
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
+import { PluginOption, defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tsconfigPaths from 'vite-tsconfig-paths'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    
+  ].filter(Boolean),
+  resolve: {
+    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+  },
+  css: {
+    modules: {
+      generateScopedName: '[name]__[local]__[hash:8]',
+      localsConvention: null,
+    },
+  },
 })
